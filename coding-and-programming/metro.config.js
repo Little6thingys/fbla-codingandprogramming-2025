1c2c8e239016d6da5f5150ddc56bb961404eac99
Yu Zheng <115912022+Little6thingys@users.noreply.github.com>
2025-02-17 15:31:19 -0800

    Revert "Merge branch 'main' of https://github.com/Little6thingys/fbla-codingandprogramming-2025"
    This reverts commit 602d7201b69f300e003612fe2ab5d40f2a05c5bb, reversing
    changes made to 65b5c8e5372fad7e7172c0aa97d20e74cce909a4.

--- coding-and-programming/metro.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    transformer: {
-      unstable_allowRequireContext: true, // Ensure Metro processes all modules correctly
-    },
-    resolver: {
-      unstable_disableInstrumentedBridgelessModuleResolution: true, // Disable bridgeless mode
-    },
-  };
-  
