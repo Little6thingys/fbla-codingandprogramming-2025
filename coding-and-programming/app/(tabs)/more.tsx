5b5f4e02d8d100175244dba84e04853ac27790bd
Yu Zheng <115912022+Little6thingys@users.noreply.github.com>
2025-02-17 15:31:43 -0800

    Revert "fix?"
    This reverts commit 65b5c8e5372fad7e7172c0aa97d20e74cce909a4.

--- coding-and-programming/app/(tabs)/more.tsx
+++ coding-and-programming/app/(tabs)/more.tsx
@@ -7,7 +7,7 @@ import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { IconSymbol } from '@/components/ui/IconSymbol';
 
-export default function MoreScreen() {
+export default function TabTwoScreen() {
   return (
     <ThemedView style={styles.backgroundContainer}>
                 
